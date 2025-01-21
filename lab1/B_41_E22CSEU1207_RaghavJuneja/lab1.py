cc1d266e5c48f1191aed17f687495ecca26bb634
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-01-21 13:31:51 +0530

    Added croped code- canvas, individual

--- lab1/B_41_E22CSEU1207_RaghavJuneja/lab1.py
+++ lab1/B_41_E22CSEU1207_RaghavJuneja/lab1.py
@@ -330,19 +330,45 @@ if __name__ == '__main__':
 
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
+    # canvas = Perform(blank_width, blank_height)
+    # canvas.make_shape(50, 50, 200, 150, shape_type='rectangle', color=(0, 255, 0))
+    # canvas.make_shape(250, 50, 400, 200, shape_type='triangle', color=(255, 0, 0))
+    # canvas.make_shape(50, 250, 200, 400, shape_type='square', color=(0, 0, 255))
+    # canvas.make_shape(250, 250, 400, 400, shape_type='rhombus', color=(255, 255, 0))
+
+    # rotated_canvas = canvas.rotate(45)
+    # cv2.imwrite(f"{output_dir}/rotated_canvas.png", rotated_canvas)
+
+    
+    # reflected_canvas = canvas.reflect('horizontal')
+    # cv2.imwrite(f"{output_dir}/reflected_canvas.png", reflected_canvas)
+
+    
+    # sheared_canvas = canvas.shear(0.5, 0.2)
+    # cv2.imwrite(f"{output_dir}/sheared_canvas.png", sheared_canvas)
+    
     canvas = Perform(blank_width, blank_height)
     canvas.make_shape(50, 50, 200, 150, shape_type='rectangle', color=(0, 255, 0))
     canvas.make_shape(250, 50, 400, 200, shape_type='triangle', color=(255, 0, 0))
     canvas.make_shape(50, 250, 200, 400, shape_type='square', color=(0, 0, 255))
     canvas.make_shape(250, 250, 400, 400, shape_type='rhombus', color=(255, 255, 0))
 
-    rotated_canvas = canvas.rotate(45)
-    cv2.imwrite(f"{output_dir}/rotated_canvas.png", rotated_canvas)
 
-    
-    reflected_canvas = canvas.reflect('horizontal')
-    cv2.imwrite(f"{output_dir}/reflected_canvas.png", reflected_canvas)
+    cropped_canvas = canvas.crop(100, 100, 400, 400)
+    cv2.imwrite(f"{output_dir}/cropped_canvas.png", cropped_canvas)
 
-    
-    sheared_canvas = canvas.shear(0.5, 0.2)
-    cv2.imwrite(f"{output_dir}/sheared_canvas.png", sheared_canvas)
+    #
+    rectangle_cropped = canvas.crop(50, 50, 200, 150)
+    cv2.imwrite(f"{output_dir}/rectangle_cropped.png", rectangle_cropped)
+
+    triangle_cropped = canvas.crop(250, 50, 400, 200)
+    cv2.imwrite(f"{output_dir}/triangle_cropped.png", triangle_cropped)
+
+
+    square_cropped = canvas.crop(50, 250, 200, 400)
+    cv2.imwrite(f"{output_dir}/square_cropped.png", square_cropped)
+
+
+    rhombus_cropped = canvas.crop(250, 250, 400, 400)
+    cv2.imwrite(f"{output_dir}/rhombus_cropped.png", rhombus_cropped)
+
--- lab1/B_41_E22CSEU1207_RaghavJuneja/tempCodeRunnerFile.py
+++ lab1/B_41_E22CSEU1207_RaghavJuneja/tempCodeRunnerFile.py
@@ -1,2 +1,25 @@
-waitKey(0)
-    cv2.destroyAllWindows()
+as = Perform(blank_width, blank_height)
+    canvas.make_shape(50, 50, 200, 150, shape_type='rectangle', color=(0, 255, 0))
+    canvas.make_shape(250, 50, 400, 200, shape_type='triangle', color=(255, 0, 0))
+    canvas.make_shape(50, 250, 200, 400, shape_type='square', color=(0, 0, 255))
+    canvas.make_shape(250, 250, 400, 400, shape_type='rhombus', color=(255, 255, 0))
+
+
+    cropped_canvas = canvas.crop(100, 100, 400, 400)
+    cv2.imwrite(f"{output_dir}/cropped_canvas.png", cropped_canvas)
+
+    #
+    rectangle_cropped = canvas.crop(50, 50, 200, 150)
+    cv2.imwrite(f"{output_dir}/rectangle_cropped.png", rectangle_cropped)
+
+    triangle_cropped = canvas.crop(250, 50, 400, 200)
+    cv2.imwrite(f"{output_dir}/triangle_cropped.png", triangle_cropped)
+
+
+    square_cropped = canvas.crop(50, 250, 200, 400)
+    cv2.imwrite(f"{output_dir}/square_cropped.png", square_cropped)
+
+
+    rhombus_cropped = canvas.crop(250, 250, 400, 400)
+    cv2.imwrite(f"{output_dir}/rhombus_cropped.png", rhombus_cropped)
+
